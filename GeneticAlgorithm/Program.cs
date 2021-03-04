4c7a837daac0370a5036ed76ac670ae25cccb190
Made <asmirnoff94@mail.ru>
2021-03-04 11:29:44 +0300

    Добавил расчет фитнеса как отношение кол-ва не занятых серверов к общему кол-ву серверов

--- GeneticAlgorithm/Program.cs
+++ GeneticAlgorithm/Program.cs
@@ -29,35 +29,8 @@ namespace GeneticAlgorithm
 				var genes = mc.GetGenes();
 				var servers = genes.Select(x => (Server) x.Value).ToList();
 
-				var freeServersTerm = servers.Count(x => x.IsFree) * 2.5;
-				var negativeHddFreeTerm = servers.Where(x => x.HddFree < 0).Select(x=>x.HddFree * 3).Sum();
-				var positiveHddFreeTerm = servers.Where(x => x.HddFree > 0).Select(x => x.HddFree * 0.2).Sum();
-				var negativeRamFreeTerm = servers.Where(x => x.RamFree < 0).Select(x => x.RamFree * 3).Sum();
-				var positiveRamFreeTerm = servers.Where(x => x.RamFree > 0).Select(x => x.RamFree * 0.2).Sum();
-				var negativeCpuFreeTerm = servers.Where(x => x.CpuFree < 0).Select(x => x.CpuFree * 3).Sum();
-				var positiveCpuFreeTerm = servers.Where(x => x.CpuFree > 0).Select(x => x.CpuFree * 0.2).Sum();
-
-				var fitness = freeServersTerm + negativeHddFreeTerm + positiveHddFreeTerm + negativeRamFreeTerm +
-				              positiveRamFreeTerm + negativeCpuFreeTerm + positiveCpuFreeTerm;
-
-				//по каждому серверу нужно вычислить коэффициент заполненности
-				var serversFill = new List<double>();
-				foreach (var server in servers)
-				{
-					var serverFill = ((server.HddFull-server.HddFree) / server.HddFull) * 0.2 +
-					((server.RamFull-server.RamFree) / server.RamFull) * 0.4 +
-					((server.CpuFull-server.CpuFree) / server.CpuFull) * 0.4;
-					serversFill.Add(serverFill);
-				}
-				serversFill = serversFill.OrderByDescending(x=>x).ToList();
-				//отладочная строка
-				//var serversFill = new List<double> { 0.9d, 0.9d, 0.6d, 0.2d,0 };
-				//среднее значение заполненности серверов
-				var averageFill = serversFill.Sum() / servers.Count();
-				//рассчитаем дисперсию у serversFill
-				var dispersion = serversFill.Select(eachFill => (eachFill - averageFill) * (eachFill - averageFill)).Sum()/servers.Count;
-
-				return (dispersion*3) + fitness;
+				var freeServers = servers.Count(x => x.IsFree);
+				return freeServers / servers.Count;
 			});
 
 			var selection = new EliteSelection();
